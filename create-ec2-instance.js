3d7a2ac34daebcc6d747bf4b98bfe967e018f013
komen <gkkomensi@gmail.com>
2019-11-14 11:01:55 +0300

    create and save keyfile

--- create-ec2-instance.js
+++ create-ec2-instance.js
@@ -76,3 +76,24 @@ function createKeyPair(keyName) {
         })
     })
 }
+
+function createInstance(sgName, keyName) {
+    const params = {
+        ImageId: 'ami-05c859630889c79c8',
+        InstanceType: 't2.micro',
+        KeyName: keyName,
+        MaxCount: 1,
+        MinCount: 1,
+        SecurityGroups: [
+            sgName
+        ],
+        UserData: 'IyEvYmluL2Jhc2gKY3VybCAtLXNpbGVudCAtLWxvY2F0aW9uIGh0dHBzOi8vcnBtLm5vZGVzb3VyY2UuY29tL3NldHVwXzgueCB8IHN1ZG8gYmFzaCAtCnN1ZG8geXVtIGluc3RhbGwgLXkgbm9kZWpzCnN1ZG8geXVtIGluc3RhbGwgLXkgZ2l0CmdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vcnlhbm11cmFrYW1pL2hiZmwuZ2l0CmNkIGhiZmwKbnBtIGkKbnBtIHJ1biBzdGFydAoK' //base64 encoding of the script
+    }
+
+    return new Promise((resolve, reject) => {
+        ec2.runInstances(params, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
+}
